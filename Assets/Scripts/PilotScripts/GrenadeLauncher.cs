262c7ef1a1bd0c777534c1c63de4a1ec5e62ab24
Marina Makram Kamel <marinamakram210@gmail.com>
2019-12-11 13:44:27 +0200

    integrate heavy weapon in new repo

---
+++ Assets/Scripts/PilotScripts/GrenadeLauncher.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrenadeLauncher : MonoBehaviour
+{
+    public GameObject grenadePrefab;
+    public float weaponForce;
+    public PilotHealth ph;
+    private bool shooted = false;
+    private int count = 100;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            count = 100;
+            ph.setAmmo(100, count);
+            shooted = false;
+        }
+
+        if (Input.GetMouseButtonDown(0) && shooted == false)
+        {
+            ShootGrenade();
+            shooted = true;
+        }
+    }
+    public void setAmmo()
+    {
+        ph.setAmmo(100, count);
+    }
+
+    void ShootGrenade()
+    {
+        count = 0;
+        ph.setAmmo(100, count);
+        GameObject grenade = Instantiate(grenadePrefab, transform.TransformPoint(0, 0.1f, 0.05f), transform.rotation);
+        //grenade.transform.Rotate(0.0f, 0.0f, 0.0f, Space.World);
+        Debug.Log(grenade.transform.position);
+        Debug.Log(transform.rotation);
+        grenade.GetComponent<Rigidbody>().useGravity = true;
+        grenade.GetComponent<Rigidbody>().AddForce(transform.forward * weaponForce, ForceMode.Impulse);
+        //destroy grenade if touched enemy, otherwise destroy grenade after specific time
+        Destroy(grenade, 3);
+    }
+}
---
+++ Assets/Scripts/grenadeExplosion.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class grenadeExplosion : MonoBehaviour
+{
+    public GameObject explosionEffect;
+    private float grenadeRadius = 4f;
+    private Collider[] shootedEnemies;
+    private GameObject instantiatedObj;
+    public LayerMask explosionLayers;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Explosion(collision.contacts[0].point);
+
+    }
+
+    void Explosion(Vector3 centerPoint)
+    {
+        shootedEnemies = Physics.OverlapSphere(centerPoint, grenadeRadius, explosionLayers);
+        foreach (Collider shootedEnemy in shootedEnemies)
+        {
+            Debug.Log(shootedEnemy.gameObject.name);
+            instantiatedObj = Instantiate(explosionEffect, transform.position, transform.rotation);
+            Destroy(instantiatedObj, 3f);
+            Target target = shootedEnemy.gameObject.GetComponent<Target>();
+            if (shootedEnemy.gameObject.tag == "EnemyTitan")
+            {
+                target.TakeDamage(125, 50);
+            }
+
+            if (shootedEnemy.gameObject.tag == "EnemyPilot")
+            {
+                target.TakeDamage(125, 10);
+            }
+            Destroy(gameObject);
+        }
+
+
+    }
+}
